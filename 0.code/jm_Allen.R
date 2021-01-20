da1ab30d2f3fdf1a777c78ebb3d87350aca33674
jjaa-mp <jmoriano@ub.edu>
2021-01-20 12:13:31 +0000

    cell type enrich stat code

--- 0.code/jm_Allen.R
+++ 0.code/jm_Allen.R
@@ -28,12 +28,22 @@ library(Matrix)
 ensembl <- useMart(biomart = 'ENSEMBL_MART_ENSEMBL', 
                         dataset = 'hsapiens_gene_ensembl',
                         host = 'https://grch37.ensembl.org')
+#Total number of protein coding genes in Ensembl
+resP=getBM(attributes = c("hgnc_symbol","gene_biotype"),
+              filters = "biotype",
+              values = list(biotype="protein_coding"), mart = ensembl)
+length(resP$hgnc_symbol[!(is.na(resP$hgnc_symbol) | resP$hgnc_symbol=="")])
+
 ##Akey Deserts coordinates (Ensembl 1-based):
 filterlist <- c("1:105400000:120600000", "3:74100000:89300000", "7:106200000:123200000","8:49400000:66500000")
 ##Select only protein-coding from Akey
 results=getBM(attributes = c("hgnc_symbol", "chromosome_name", "start_position", "end_position","gene_biotype"),
               filters = c("chromosomal_region","biotype"),
               values = list(chromosomal_region=filterlist,biotype="protein_coding"), mart = ensembl)
+
+#255
+length(results$hgnc_symbol[!(is.na(results$hgnc_symbol) | results$hgnc_symbol=="")])
+
 
 #ALTERNATIVE: 800 genes (protein coding genes plus other genes)
 ##results_alternative =getBM(attributes = c("hgnc_symbol", "chromosome_name", "start_position", "end_position"),
@@ -704,140 +714,13 @@ meanakeypey[order(meanakeypey$Mean, decreasing = TRUE),]
 #Raw:
 df  %>% group_by(organ) %>% summarize(Mean = mean(max.expr), .groups = 'drop')
 
-df1 <- df[which(df$organ=='Cerebellum'), ]
-brain <- df[which(df$organ=='Cerebrum'), ]
-#write.csv(df1, file="CellAtlas_GSE156793_cerebellum.csv", row.names = FALSE)
-
-# CBL - AKEY
-df1$gene_short_name <- gsub("\\'", "", df1$gene_short_name)
-df1subset <- df1[df1$gene_short_name %in% results$hgnc_symbol,]
-df1subset <- df1subset[order(df1subset$max.cluster),]
-# CBL - AKEY & PEY
-df1subsetboth <- df1[df1$gene_short_name %in% both$hgnc_symbol,]
-df1subsetboth <- df1subsetboth[order(df1subsetboth$max.cluster),]
-df1subset <- df1subset[order(df1subset$max.cluster),]
-#Cerebellum FILES:
-#write.csv(df1subset, file="CellAtlas_GSE156793_CBL_inAkey.csv", row.names = FALSE)
-#write.csv(df1subsetboth, file="CellAtlas_GSE156793_CBL_inAkeyPey.csv", row.names = FALSE)
-```
-
-#Friedman test - Genes in Akey:
-```{r}
-r <- data.frame(dfakey$organ, dfakey$gene_short_name, dfakey$max.expr)
-names(r) <- c("x", "y", "z")
-rr <- pivot_wider(r, names_from = x, values_from = z)
-rr3 <- as.matrix(rr)
-rr3 <-rr3[,-1]
-friedman.test(rr3)
-
-list1 <- vector(mode = "list")
-for (i in 1:length(colnames(rr3))){
-  list1[[i]] <- as.numeric(c(rr3[,i]))
-}
-res_F <- DunnettTest(list1, control = c(1:15)) #CAN ALSO TRY SNK Test
-organs <- colnames(rr3)
-list_dfs <- vector(mode="list")
-for (i in 1:length(res_F)){
-  colnames(res_F[[i]]) <- c("diff", "lwr.ci", "upr.ci", colnames(rr3)[i]) #control group: colnames(rr3)[i]
-  rownames(res_F[[i]]) <- organs[-i] #comparison groups in rownames: all except i
-  list_dfs[[i]] <- as.data.frame(res_F[[i]])
-  list_dfs[[i]] <- list_dfs[[i]][4]
-  list_dfs[[i]] <-  cbind(list_dfs[[i]], group=rownames(list_dfs[[i]]))
-  list_dfs[[i]] <- list_dfs[[i]][c(2,1)] #ordering columns for merging on index 'group'
-}
-pp <-reduce(list_dfs, full_join, by="group") #Generating matrix for plot
-pp[is.na(pp)] <- 2 #replacing null values
-rownames(pp) <- pp[,1]
-pp[,1] <- NULL
-pp <- pp[c(2:15,1)] #Reordering columns for triangular matrix
-
-#Plot
-##Cell Atlas - Akey - Enrichment
-
-  # Get upper triangle of the correlation matrix
-  get_upper_tri <- function(cormat){
-    cormat[lower.tri(cormat)]<- NA
-    return(cormat)
-  }
-upper_tri <- get_upper_tri(as.matrix(pp))
-melted <- melt(upper_tri, na.rm = TRUE)
-
-pl_fr <- ggplot(data = melted, aes(Var2, Var1, fill = value))+
- geom_tile(color = "white")+
- scale_fill_gradient2(low = "#F0E442", high = "#0072B2", mid = "#CC79A7", 
-   midpoint = 0.5, limit = c(0,1), space = "Lab", 
-   name="p-value") +
-  theme_classic()+xlab("")+ylab("")+ 
- theme(axis.text.x = element_text(angle = 45,hjust = 0))+scale_y_discrete(position = "right")+
- coord_fixed()+ coord_flip()
-pl_fr
-ggsave(file="CellAtlas_MeanExprAkey_heatmap.pdf", pl_fr,width = 11.69, height = 8.27, units = "in")
-
-```
-
-#Friedman test - Genes in both Akey and Pey:
-```{r}
-s <- data.frame(dfsubsetboth$organ, dfsubsetboth$gene_short_name, dfsubsetboth$max.expr)
-names(s) <- c("x", "y", "z")
-ss <- pivot_wider(s, names_from = x, values_from = z)
-ss3 <- as.matrix(ss)
-ss3 <-ss3[,-1]
-friedman.test(ss3)
-
-list2 <- vector(mode = "list")
-for (i in 1:length(colnames(ss3))){
-  list2[[i]] <- as.numeric(c(ss3[,i]))
-}
-res_F2 <- DunnettTest(list2, control = c(1:15)) #CAN ALSO TRY SNK Test
-organs <- colnames(ss3)
-
-list_dfs2 <- vector(mode="list")
-for (i in 1:length(res_F2)){
-  colnames(res_F2[[i]]) <- c("diff", "lwr.ci", "upr.ci", colnames(ss3)[i]) #control group: colnames(ss3)[i]
-  rownames(res_F2[[i]]) <- organs[-i] #comparison groups in rownames: all except i
-  list_dfs2[[i]] <- as.data.frame(res_F2[[i]])
-  list_dfs2[[i]] <- list_dfs2[[i]][4]
-  list_dfs2[[i]] <-  cbind(list_dfs2[[i]], group=rownames(list_dfs2[[i]]))
-  list_dfs2[[i]] <- list_dfs2[[i]][c(2,1)] #ordering columns for merging on index 'group'
-}
-
-pp2 <-reduce(list_dfs2, full_join, by="group") #Generating matrix for plot
-pp2[is.na(pp2)] <- 2 #replacing null values
-rownames(pp2) <- pp2[,1]
-pp2[,1] <- NULL
-pp2 <- pp2[c(2:15,1)] #Reordering columns for triangular matrix
-
-#Plot
-##Cell Atlas - Akey - Enrichment
-
-# Get upper triangle of the correlation matrix
-get_upper_tri <- function(cormat){
-cormat[lower.tri(cormat)]<- NA
-return(cormat)
-}
-upper_tri2 <- get_upper_tri(as.matrix(pp2))
-melted2 <- melt(upper_tri2, na.rm = TRUE)
-
-pl_fr2 <- ggplot(data = melted2, aes(Var2, Var1, fill = value))+
- geom_tile(color = "white")+
- scale_fill_gradient2(low = "#F0E442", high = "#0072B2", mid = "#CC79A7", 
-   midpoint = 0.5, limit = c(0,1), space = "Lab", 
-   name="p-value") +
-  theme_classic()+xlab("")+ylab("")+ 
- theme(axis.text.x = element_text(angle = 45,hjust = 0))+scale_y_discrete(position = "right")+
- coord_fixed()+ coord_flip()
-pl_fr2
-ggsave(file="CellAtlas_MeanExprAkeyPey_heatmap.pdf", pl_fr2,width = 11.69, height = 8.27, units = "in")
-```
-
-#CELL ATLAS - CELL TYPE ENRICHMENT
-```{r}
-#CEREBRUM - AKEY
+#CEREBRUM - Hyper geom
 brain$gene_short_name <- gsub("\\'", "", brain$gene_short_name)
 brain_akey <- brain[brain$gene_short_name %in% results$hgnc_symbol,]
 ctb <- data.frame(brain_akey$max.cluster, brain_akey$gene_short_name, brain_akey$max.expr)
 names(ctb) <- c("x", "y", "z")
 ctb1 <- pivot_wider(ctb, names_from = x, values_from = z)
+colSums(!is.na(ctb1))#sample size 228
 z0 <- as.data.frame(colSums(!is.na(ctb1[,-1]))) #removing y
 names(z0) <- "Akey genes"
 setDT(z0, keep.rownames = "Var1")
@@ -847,19 +730,61 @@ gr <- data.frame(brain$max.cluster, brain$gene_short_name, brain$max.expr)
 names(gr) <- c("x", "y", "z")
 gr1 <- as.data.frame(table(gr$x))
 gr1<-gr1[which(gr1$Freq != 0),]
-names(gr1) <- c("Var1", "Total DEG")
-
+names(gr1) <- c("Var1", "Total Cerebrum DEG")
 finaltable <- merge(gr1, z0, by="Var1")
+##Hyper geom:
+finaltable$pvalue <- phyper(finaltable$`Akey genes`-1, 255, 19238-255, finaltable$`Total Cerebrum DEG`, lower.tail = FALSE, log.p = FALSE)
 
-
-
-#CEREBELLUM - AKEY
+#CBL - Hyper geom
 df1$gene_short_name <- gsub("\\'",  "", df1$gene_short_name)
 cbl_akey <- df1[df1$gene_short_name %in% results$hgnc_symbol,]
 cblt <- data.frame(cbl_akey$max.cluster, cbl_akey$gene_short_name, cbl_akey$max.expr)
 names(cblt) <- c("x", "y", "z")
 cblt1 <- pivot_wider(cblt, names_from = x, values_from = z)
 colSums(!is.na(cblt1))
+cblt1
+colSums(!is.na(cblt1))
+z1 <- as.data.frame(colSums(!is.na(cblt1[,-1]))) #removing y
+names(z1) <- "Akey genes"
+setDT(z1, keep.rownames = "Var1")
+z1 <- as.data.frame(z1)
+#testing
+fi1 <- data.frame(df1$max.cluster, df1$gene_short_name, df1$max.expr)
+names(fi1) <- c("x", "y", "z")
+fi1 <- as.data.frame(table(fi1$x))
+fi1<-fi1[which(fi1$Freq != 0),]
+#testing
+fi1 <- data.frame(df1$max.cluster, df1$gene_short_name, df1$max.expr)
+names(fi1) <- c("x", "y", "z")
+fi1 <- as.data.frame(table(fi1$x))
+fi1<-fi1[which(fi1$Freq != 0),]
+names(fi1) <- c("Var1", "Total CBL DEG")
+finaltable2 <- merge(fi1, z1, by="Var1")
+finaltable2
+finaltable2$pvalue <- phyper(finaltable2$`Akey genes`-1, 255, 19238-255, finaltable2$`Total CBL DEG`, lower.tail = FALSE, log.p = FALSE)
+
+
+#EYE - Hyper geom
+eye <- df[which(df$organ=='Eye'), ]
+eye$gene_short_name <- gsub("\\'",  "", eye$gene_short_name)
+eye_akey <- eye[eye$gene_short_name %in% results$hgnc_symbol,]
+eyelt <- data.frame(eye_akey$max.cluster, eye_akey$gene_short_name, eye_akey$max.expr)
+names(eyelt) <- c("x", "y", "z")
+eyelt <- pivot_wider(eyelt, names_from = x, values_from = z)
+colSums(!is.na(eyelt)) #Sample size 215
+z2 <- as.data.frame(colSums(!is.na(eyelt[,-1]))) #removing y
+names(z2) <- "Akey genes"
+setDT(z2, keep.rownames = "Var1")
+z2 <- as.data.frame(z2)
+#testing
+fi2 <- data.frame(eye$max.cluster, eye$gene_short_name, eye$max.expr)
+names(fi2) <- c("x", "y", "z")
+fi2 <- as.data.frame(table(fi2$x))
+fi2<-fi2[which(fi2$Freq != 0),]
+names(fi2) <- c("Var1", "Total EYE DEG")
+finaltable3 <- merge(fi2, z2, by="Var1")
+#hyper geom test
+finaltable3$pvalue <- phyper(finaltable3$`Akey genes`-1, 255, 19238-255, finaltable3$`Total EYE DEG`, lower.tail = FALSE, log.p = FALSE)
 
 #CEREBRUM - AKEY & PEY
 #brain$gene_short_name <- gsub("\\'", "", brain$gene_short_name)
@@ -875,7 +800,6 @@ cbl_akeypey1 <- data.frame(cbl_akeypey$max.cluster, cbl_akeypey$gene_short_name,
 names(cbl_akeypey1) <- c("x", "y", "z")
 cbl_akeypey2 <- pivot_wider(cbl_akeypey1, names_from = x, values_from = z)
 colSums(!is.na(cbl_akeypey2))
-
 ```
 
 
