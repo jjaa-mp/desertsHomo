2c14768c06db2834a59cc5ba0f955d0388b89b69
AGMAndirko <sanyi.personal@gmail.com>
2021-01-21 17:51:20 +0100

    Added anova function 	- Added observation re anova 	- Prepared data for combined testing 	- Changed data function to accomodate not returning means when it's not a plot

--- 0.code/R_scripts/R/GSE_data.R
+++ 0.code/R_scripts/R/GSE_data.R
@@ -1,4 +1,4 @@
-GSE_data <- function (results, akeypey, out) {
+GSE_data <- function (results, relevantregion, out) {
   df <- read.csv(file = "file_dependencies/GSE156793_S8_DE_gene_cells.csv.gz")
   df1 <- df[which(df$organ=='Cerebellum'), ]
   brain <- df[which(df$organ=='Cerebrum'), ]
@@ -17,12 +17,12 @@ GSE_data <- function (results, akeypey, out) {
   return(dfakey)
 } else if (out == "akeypey"){
     
-    df1subsetboth <- df1[df1$gene_short_name %in% akeypey$hgnc_symbol,]
+    df1subsetboth <- df1[df1$gene_short_name %in% relevantregion$hgnc_symbol,]
     df1subsetboth <- df1subsetboth[order(df1subsetboth$max.cluster),]
     df1subset <- df1subset[order(df1subset$max.cluster),]
     write.csv(df1subsetboth, file="output/CellAtlas_GSE156793_inAkeyPey.csv", row.names = FALSE)
     #Whole dataset - AKEY PEY
-    df1subsetboth <- df[df$gene_short_name %in% akeypey$hgnc_symbol,]
+    df1subsetboth <- df[df$gene_short_name %in% relevantregion$hgnc_symbol,]
     #meanakeypey <- df1subsetboth %>% group_by(organ) %>% summarize(Mean = mean(max.expr), .groups = 'drop')
     #meanakeypey[order(meanakeypey$Mean, decreasing = TRUE),]
     return(df1subsetboth)
--- Observations.md
+++ Observations.md
@@ -2,3 +2,11 @@
 ## Highlights from the data analysis
 
 Key for me: make clear why we focus on Akey, why we further focus on Pey (I suggest we just focus on the 'extended', not 'core' set), and point to differences from the early, tentative Vernot conclusion
+
+
+## Stats
+
+## Anova
+In an nested anova derived from the data from the Chen + Pey data, cerebellum and striatum have significantly different gene expression values (as expected from the plots) (p = 0.035). That is not the case in the Chen data only (0.4415).
+
+In a comparison between Chen + Pey and Chen only... 
